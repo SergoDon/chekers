791d4ee36a7eafd075c766b0952bedf311874a7e
unknown <ssdonchenko@gmail.com>
2022-05-10 21:34:44 +0200

    MnogaKoda

--- genBoard.js
+++ genBoard.js
@@ -1,32 +1,51 @@
+let blackColor = `rgb(0,0,0)`
+let whiteColor = `rgb(255,255,255)`
+
+let checkerGrayColor = `rgb(128,128,128)`
+let checkerRedColor = `rgb(168, 30, 30)`
+
+let checkerYellowBg = `rgb(255, 240, 50)`
+let checkerBlackBg = `rgb(0, 0, 0)`
+
+let arrLetters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H']
+let cellInMemory = null
+let elementInMemory = null
+let gameHistory = []//{userId, idFirstCell, idNextCell}
+let users = [] //{ username, userId, checkerColor }
+let userId = null
+
+const genBoard = generateChekersBoard();
+
+function curetnUser () {
+   const user = users.find( (user) => {
+      return user.userId === userId
+   })
+   return user
+}
+
 function generateChekersBoard() {
 
-   let blackColor = `rgba(0,0,0,1)`
-   let whiteColor = `rgba(255,255,255,1)`
-
-   let checkerGrayColor = `rgba(190, 190, 190, 1)`
-   let checkerRedColor = `rgba(168, 30, 30, 1)`
-
-   let checkerYellowBg = `rgba(255, 240, 0, 1)`
-   let checkerBlackBg = `rgba(0, 0, 0, 1)`
-
-   let arrLetters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H']
-   let arrLength = arrLetters.length;
+let arrLength = arrLetters.length;
 
 let line = arrLetters.map( (values, index, arr) => {
    
    let board = arr.map((arrValue, arrIndex) => {
-      let x = values;
+      let x = index+1
+      let letter = values;
       let y = arrLength - arrIndex;
       let colorCell = null;
       let isExist = null;
       let colorCheker = null;
-
-      //(arrIndex%2 && x === 'A' || x === 'C' || x === 'E' || x === 'G') ? colorCell = blackColor : colorCell = whiteColor;
-      
+      let isDisable = true
+     
       if(index%2){ 
           colorCell = arrIndex%2 ? whiteColor : blackColor;
+          isDisable = arrIndex%2 ? false : isDisable
       }  
-      else colorCell = arrIndex%2 ? blackColor : whiteColor;
+      else {
+         colorCell = arrIndex%2 ? blackColor : whiteColor;
+         isDisable = arrIndex%2 ? isDisable : false
+      } 
       
       if( arrIndex+1 < 4 ){
          colorCheker = colorCell === blackColor ? checkerGrayColor : null;
@@ -36,14 +55,16 @@ let line = arrLetters.map( (values, index, arr) => {
          colorCheker = colorCell === blackColor ? checkerRedColor : null;
          isExist = colorCell === blackColor; 
       }
-      let cheker = {colorCheker, isExist, isSelected: false, isKing: false}
+      let cheker = {userId: null, colorCheker, isExist, isSelected: false, isKing: false}
 
       arrValue = {
+         letter,
          x, 
          y, 
-         title: `${x}${y}`,
+         id: `${letter}${y}`,
          colorCell,
-         cheker
+         cheker,
+         isDisable,
       }
       return arrValue;
    })
@@ -54,11 +75,8 @@ let line = arrLetters.map( (values, index, arr) => {
 return line;
 }
 
-const genBoard = generateChekersBoard();
-console.log(genBoard)
-
-
 function genletNumLine (chekenBoard, divType) {
+   
    const line = document.createElement('div')
    
    chekenBoard.forEach((element, indexCb) => {
@@ -70,7 +88,7 @@ function genletNumLine (chekenBoard, divType) {
          line.classList.add('letterLine')
          lineElement.classList.add('letters')
          element.forEach( (elementItem) => {
-            lineElement.innerText = elementItem.x
+            lineElement.innerText = elementItem.letter
          })
       } 
       else { 
@@ -82,8 +100,114 @@ function genletNumLine (chekenBoard, divType) {
    return line
 }
 
-function cellClick() {
-   console.log('123')
+function checkingCorrectMove (startCell, nextCell) {
+   
+   const {x: nextChekerX, y: nextChekerY} = nextCell//координаты ячейки на которую кликнули
+   const {cheker: { colorCheker }, x: startChekerX, y: startChekerY } = startCell//цевет шашки которой ходим
+
+   
+   
+   if(colorCheker === checkerGrayColor) {
+      if ( (startChekerY - nextChekerY) > 1 || (startChekerY - nextChekerY) < 0) return true
+      if ( (startChekerX - nextChekerX) > 1 || (nextChekerX - startChekerX) > 1 ) return true
+   }
+
+   if(colorCheker === checkerRedColor) {
+      console.log(startCell)
+      if ( (nextChekerY - startChekerY) > 1 || (nextChekerY - startChekerY) < 0) return true
+      if ( (startChekerX - nextChekerX) > 1 || (nextChekerX - startChekerX) > 1 ) return true
+   }
+}
+
+function giveNewUserId (){
+   let newId = curetnUser().userId
+   let i = 0
+   while (newId === userId){
+      newId = users[i].userId
+      i++
+   }
+   userId = newId
+}
+
+function cellClick(id) {
+   
+   const selectedElement = document.getElementById(id)
+   const userLine = document.getElementsByClassName('curentUser')
+   const divCheker = selectedElement.childNodes[0]
+   const boardInArr = genBoard.flat()
+   const curentUser = curetnUser()
+
+   const selectedСell = boardInArr.find( (selectedElement) => {
+      return selectedElement.id === id
+   } )
+   
+   const selectCheker = () => {
+      if(divCheker) {
+         elementInMemory = document.getElementById(id)
+         cellInMemory = boardInArr.find( (selectedElement) => {
+            return selectedElement.id === id
+         } )
+
+         if(cellInMemory.cheker.userId !== curentUser.userId) {
+            cellInMemory = null
+            return
+         } 
+
+         cellInMemory.cheker.isSelected = true
+         selectedElement.style.backgroundColor = checkerYellowBg
+      } 
+   }
+
+   
+
+   if(cellInMemory && divCheker) {
+      elementInMemory.style.backgroundColor = blackColor
+      selectCheker()
+      selectedElement.style.backgroundColor = blackColor
+          
+      cellInMemory = null
+      return     
+   } 
+
+   if(divCheker) {
+      selectCheker()
+   }   
+
+   else if (selectedСell.isDisable && cellInMemory) {
+      if(cellInMemory.cheker.isSelected && !divCheker) {   
+         if (checkingCorrectMove (cellInMemory, selectedСell)) return
+         
+         selectedСell.cheker.colorCheker = cellInMemory.cheker.colorCheker
+         
+         const elUserHistory = document.getElementsByName('gameHistory')
+         const checker = document.createElement('div')
+         const childElementInMemory = elementInMemory.childNodes[0]
+
+         checker.classList.add('cheker')
+         checker.style.backgroundColor = selectedСell.cheker.colorCheker
+         elementInMemory.style.backgroundColor = blackColor
+
+         cellInMemory.cheker.isSelected = false
+         selectedСell.cheker.userId = curentUser.userId
+         cellInMemory.cheker.userId = null
+
+         selectedElement.appendChild(checker)
+         elementInMemory.removeChild(childElementInMemory)
+
+         cellInMemory = null
+        
+         giveNewUserId()
+
+         userLine[0].innerText = curetnUser().userName
+         userLine[0].setAttribute('style', `color:${curetnUser().chekerColor}`)
+         //elUserHistory[0].innerText = 
+      
+
+         history(curentUser.userId, curentUser.userName, elementInMemory.id, selectedElement.id)
+
+         elUserHistory[0].innerText = `${curentUser.userName}: ${curentUserHistor(curentUser)}`
+      }  
+   } 
 }
 
 function renderBoard(chekenBoard) {
@@ -116,16 +240,20 @@ function renderBoard(chekenBoard) {
 
       item.forEach((itemCell) => {
 
-         const {colorCell, cheker: {isExist, colorCheker}} = itemCell
+         const {colorCell, id, isDisable, cheker: {isExist, colorCheker}} = itemCell
 
-         const cell = document.createElement('div')      
+         const cell = document.createElement('div') 
+
          cell.classList = 'itemCell'
          cell.style.backgroundColor = colorCell
-         cell.onclick = () => cellClick ()
+         cell.onclick = () => cellClick(id)
+         cell.setAttribute('id', id)
+         
+         
 
          line.appendChild(cell)
 
-         if(isExist){
+         if(isExist) {
             const chekerOnItem = document.createElement('div')
             chekerOnItem.style.background = colorCheker
             chekerOnItem.classList.add('cheker')
@@ -137,7 +265,71 @@ function renderBoard(chekenBoard) {
       border.appendChild(line)
    })
 }
-renderBoard(genBoard);
+
+function renderUser(){
+   const user = curetnUser ()
+   
+   const elem = document.getElementById('chekersBody')
+   const curentUser = document.createElement('div')
+   curentUser.innerText = user.userName
+   curentUser.style.color = user.checkerColor
+
+
+   curentUser.classList.add('curentUser')
+   elem.appendChild(curentUser)
+}
+
+addNewPlayers ('Artem')
+
+function addNewPlayers (userName){
+   
+   let chekerColor = !!users.length ? checkerRedColor : checkerGrayColor
+
+   userId = uuidv4()
+   
+   genBoard.forEach( (element) => {
+      element.forEach( (el, index) => {
+         if(users.length < 1) {
+            if(index+1 < 4) el.cheker.userId = userId
+         } else if(index+1 > 5) el.cheker.userId = userId
+      } )
+   })
+
+   users.push( {userName, userId, chekerColor} )
+
+   if(users.length < 2) { 
+      addNewPlayers ('Sergey')
+      
+   } else {
+      renderUser()
+      renderBoard(genBoard)
+      renderGameHistory()
+   }
+}
+
+function history(userId, userName, idStartCell, idFinishCell){
+   gameHistory.push( {userId, userName, idStartCell, idFinishCell} )
+}
+
+function renderGameHistory(){
+   const element = document.getElementById('chekersBody')
+   const elUserHistory = document.createElement('div')
+   elUserHistory.classList.add('elUserHistory')
+   elUserHistory.setAttribute('name', 'gameHistory' )
+
+   element.appendChild(elUserHistory)
+}
+
+function curentUserHistor(curentUser){
+   let text = ""
+   gameHistory.forEach( (qwe) => {
+      if (qwe.userId === curentUser.userId){
+         text = text + '|' + qwe.idStartCell + '>>>' + qwe.idFinishCell + '|'
+      }
+   })
+   return text
+}
+  
 
 
 
